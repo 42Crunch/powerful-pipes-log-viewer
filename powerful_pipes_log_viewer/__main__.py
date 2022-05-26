37aee8524c284368c841968b60242b47678cee9a
cr0hn <cr0hn@cr0hn.com>
2022-05-26 13:56:22 +0200

    fix - removed default action

--- powerful_pipes_log_viewer/__main__.py
+++ powerful_pipes_log_viewer/__main__.py
@@ -7,15 +7,10 @@ from powerful_pipes_log_viewer.action_help import setup_action_help
 from powerful_pipes_log_viewer.action_show import setup_action_show
 
 
-@click.group(invoke_without_command=True)
+@click.group()
 @click.pass_context
 def cli(ctx):
-    if ctx.invoked_subcommand is None:
-        default_command(ctx)
-
-    elif ctx.invoked_subcommand == "help":
-        click.echo(ctx.get_help())
-        sys.exit(0)
+    ...
 
 setup_action_list(cli)
 setup_action_show(cli)
